93a0de462a5a8976a4e57e6322f11574c999042e
pavan-upadhya <pavan.upadhya@hpe.com>
2020-04-05 19:29:13 +0530

    Added Port in the comment

--- Main.go
+++ Main.go
@@ -6,8 +6,8 @@ import(
 )
 
 func main(){
-	// Init Router
+	// Init Router. 
 	r := GetAndRegisterRouter()
-	//Start the server
+	//Start the server on port 8090
 	log.Fatal(http.ListenAndServe(":8090", r))
 }
